3a7298611c523f7d2d44acada267e4f6abfc46b7
hwong0305 <hwong.0305@gmail.com>
2019-12-29 17:46:37 -0800

    Added login router to authRouter

--- Server/auth/crypto.js
+++ Server/auth/crypto.js
@@ -5,3 +5,7 @@ const SALT_ROUNDS = 16;
 export const hashPassword = password => {
   return bcrypt.hash(password, SALT_ROUNDS);
 };
+
+export const comparePassword = (password, hash) => {
+  return bcrypt.compare(password, hash);
+};
--- Server/routers/authRouter.js
+++ Server/routers/authRouter.js
@@ -1,9 +1,28 @@
 import express from 'express';
-import { hashPassword } from '../auth/crypto';
+import { hashPassword, comparePassword } from '../auth/crypto';
 import db from '../db';
 
 const { User } = db;
 const authRouter = express.Router();
+
+authRouter.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(400).send('User does not exist');
+    }
+    const { password: hash } = user;
+    const match = comparePassword(password, hash);
+    if (!match) {
+      return res.status(400).send('Invalid Password');
+    }
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 authRouter.post('/register', async (req, res) => {
   try {
@@ -14,8 +33,9 @@ authRouter.post('/register', async (req, res) => {
     }
     const hash = await hashPassword(password);
     const user = await User.create({ username, password: hash });
-    res.json(user);
+    return res.json(user);
   } catch (err) {
+    console.log(err);
     res.status(500).send('Internal Server Error');
   }
 });
