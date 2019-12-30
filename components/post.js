64445d3fbaa7ff8a334a0ef8421b8f161632aed1
hwong0305 <hwong.0305@gmail.com>
2019-12-29 17:23:54 -0800

    Refactored table css in jsx

--- components/post.js
+++ components/post.js
@@ -1,3 +1,5 @@
+import { PostStyle } from '../util/style';
+
 const Post = () => {
   const data = [
     {
@@ -19,67 +21,7 @@ const Post = () => {
   ];
   return (
     <div>
-      <style jsx>
-        {`
-          h1 {
-            text-align: center;
-          }
-          table {
-            border-collapse: collapse;
-            text-align: left;
-            width: 100%;
-          }
-          table tr {
-            border-bottom: 1px solid;
-          }
-          table th {
-            text-transform: uppercase;
-          }
-          table th,
-          table td {
-            padding: 10px 20px;
-          }
-          table td span {
-            background: #eee;
-            color: dimgrey;
-            display: none;
-            font-size: 10px;
-            font-weight: bold;
-            padding: 5px;
-            position: absolute;
-            text-transform: uppercase;
-            top: 0;
-            left: 0;
-          }
-          tbody tr:hover {
-            background: #eee;
-          }
-          @media (max-width: 760px) {
-            table thead {
-              left: -9999px;
-              position: absolute;
-              visibility: hidden;
-            }
-            table tr {
-              border-bottom: 0;
-              display: flex;
-              flex-direction: row;
-              flex-wrap: wrap;
-              margin-bottom: 40px;
-            }
-            table td {
-              border: 1px solid;
-              margin: 0 -1px -1px 0;
-              padding-top: 35px;
-              position: relative;
-              width: 50%;
-            }
-            table td span {
-              display: block;
-            }
-          }
-        `}
-      </style>
+      <PostStyle />
       <h1>Hello User!</h1>
       <table style={{ marginTop: '3rem' }}>
         <thead>
