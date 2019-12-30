bcdc932bc3a18b1d12c7e46c183169b4b6bc6ba6
hwong0305 <hwong.0305@gmail.com>
2019-12-29 18:27:37 -0800

    Added posts from admin route

--- Server/routers/adminRouter.js
+++ Server/routers/adminRouter.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import db from '../db';
 
-const { User } = db;
+const { Post, User } = db;
 
 const adminRouter = express.Router();
 
 adminRouter.get('/users', async (req, res) => {
   try {
-    const users = await User.findAll();
+    const users = await User.findAll({ include: [{ all: true }] });
     console.log(users);
     res.json(users || {});
+  } catch (err) {
+    res.status(500).send('Error getting users');
+  }
+});
+
+adminRouter.get('/posts', async (req, res) => {
+  try {
+    const posts = await Post.findAll({ include: [{ all: true }] });
+    console.log(posts);
+    res.json(posts || {});
   } catch (err) {
     res.status(500).send('Error getting users');
   }
