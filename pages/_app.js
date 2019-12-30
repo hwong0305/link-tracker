fa39d33518792f21a24a3013d380b547ade89745
hwong0305 <hwong.0305@gmail.com>
2019-12-30 02:25:40 -0800

    Added token to React Context

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ export default function App(props) {
   const { Component, pageProps } = props;
   const [user, setUser] = useState('');
   const [loggedIn, changeLoggedIn] = useState(false);
+  const [token, setToken] = useState('');
   return (
     <Fragment>
       <Head>
@@ -17,7 +18,9 @@ export default function App(props) {
         />
       </Head>
       <GlobalStyle />
-      <AuthContext.Provider value={{ user, loggedIn, setUser, changeLoggedIn }}>
+      <AuthContext.Provider
+        value={{ user, loggedIn, setUser, changeLoggedIn, token, setToken }}
+      >
         <Component {...pageProps} />
       </AuthContext.Provider>
     </Fragment>
