ccc5038e48bd3ecb17fec02cc5c2f565f724a0b7
hwong0305 <hwong.0305@gmail.com>
2019-12-29 18:51:04 -0800

    removed console.log

--- Server/routers/adminRouter.js
+++ Server/routers/adminRouter.js
@@ -8,7 +8,6 @@ const adminRouter = express.Router();
 adminRouter.get('/users', async (req, res) => {
   try {
     const users = await User.findAll({ include: [{ all: true }] });
-    console.log(users);
     res.json(users || {});
   } catch (err) {
     res.status(500).send('Error getting users');
@@ -18,7 +17,6 @@ adminRouter.get('/users', async (req, res) => {
 adminRouter.get('/posts', async (req, res) => {
   try {
     const posts = await Post.findAll({ include: [{ all: true }] });
-    console.log(posts);
     res.json(posts || {});
   } catch (err) {
     res.status(500).send('Error getting users');
