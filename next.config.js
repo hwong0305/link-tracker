d401ef8cdee71edac73065f67b7d7e214e7b699a
hwong0305 <hwong.0305@gmail.com>
2020-01-05 17:46:51 -0800

    Added env config for next.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    API_URL: process.env.API_URL,
+    CLIENT_URL: process.env.CLIENT_URL,
+    PORT: process.env.PORT,
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PW: process.env.DB_PW,
+    SECRET: process.env.SECRET,
+    URL_SECRET: process.env.URL_SECRET,
+    ADMIN_KEY: process.env.ADMIN_KEY,
+  },
+};
